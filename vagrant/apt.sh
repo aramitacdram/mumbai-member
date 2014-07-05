af4c13508ab443f5dd17438f95bcee20d4c2f40f
Tero Keinänen <kenkku@kenkku.net>
2014-07-05 15:41:41 +0300

    basic Vagrantfile

---
+++ vagrant/apt.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+apt-get update
+apt-get install --yes \
+	texlive \
+	texlive-latex-extra \
+	texlive-xetex \
+	texlive-math-extra \
+	texlive-fonts-extra \
+	xindy \
+	texlive-lang-european
